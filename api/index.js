e4e9e5309d2ad6ed316f43100f26cf89124765c1
Shubh Gupta <shubh056888@gmail.com>
2026-04-21 19:53:43 +0530

    fix: harden Vercel API bootstrap

--- api/index.js
+++ api/index.js
@@ -1,22 +1,49 @@
-const app = require('../server/index')
+let cachedApp = null
 
-module.exports = (req, res) => {
-  const requestUrl = new URL(req.url, 'http://localhost')
-  const rewrittenPath = requestUrl.searchParams.get('path') || ''
-
-  requestUrl.searchParams.delete('path')
-
-  const normalizedPath = rewrittenPath
-    .split('/')
-    .filter(Boolean)
-    .join('/')
-
-  req.url = normalizedPath ? `/api/${normalizedPath}` : '/api'
-
-  const queryString = requestUrl.searchParams.toString()
-  if (queryString) {
-    req.url += `?${queryString}`
+const getApp = () => {
+  if (!cachedApp) {
+    cachedApp = require('../server/index')
   }
 
-  return app(req, res)
+  return cachedApp
+}
+
+module.exports = (req, res) => {
+  try {
+    const requestUrl = new URL(typeof req?.url === 'string' ? req.url : '/api', 'http://localhost')
+    const rewrittenPath = requestUrl.searchParams.get('path')
+
+    if (rewrittenPath) {
+      requestUrl.searchParams.delete('path')
+
+      const normalizedPath = rewrittenPath
+        .split('/')
+        .filter(Boolean)
+        .join('/')
+
+      req.url = normalizedPath ? `/api/${normalizedPath}` : '/api'
+
+      const queryString = requestUrl.searchParams.toString()
+      if (queryString) {
+        req.url += `?${queryString}`
+      }
+    }
+
+    return getApp()(req, res)
+  } catch (error) {
+    console.error('api bootstrap error', error)
+
+    if (res?.headersSent) {
+      return
+    }
+
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(
+      JSON.stringify({
+        error: 'API bootstrap failed.',
+        details: error?.message || String(error),
+      })
+    )
+  }
 }
