9d70b092fe4604f45207b115373b2805c4d1388a
Shubh Gupta <shubh056888@gmail.com>
2026-04-21 19:59:23 +0530

    fix: surface API bootstrap failures

--- api/index.js
+++ api/index.js
@@ -1,3 +1,30 @@
-const app = require('../server/index')
+let cachedApp = null
 
-module.exports = app
+const getApp = () => {
+  if (!cachedApp) {
+    cachedApp = require('../server/index')
+  }
+
+  return cachedApp
+}
+
+module.exports = (req, res) => {
+  try {
+    return getApp()(req, res)
+  } catch (error) {
+    console.error('api bootstrap error', error)
+
+    if (res?.headersSent) {
+      return
+    }
+
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(
+      JSON.stringify({
+        error: 'API bootstrap failed.',
+        details: error?.message || String(error),
+      })
+    )
+  }
+}
